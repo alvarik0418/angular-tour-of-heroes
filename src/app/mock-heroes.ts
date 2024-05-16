4982cd8c54613da7e46d0fab3d76e663a83d14a8
Alvaro Manjarres <alvarik0418@gmail.com>
2024-05-15 20:38:51 -0500

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+{id:12,nombre:'Dr. Nice'},
+{id:13,nombre:'Bombasto'},
+{id:14,nombre:'Celeritas'},
+{id:15,nombre:'Magneta'},
+{id:16,nombre:'RubberMan'},
+{id:17,nombre:'Dynama'},
+{id:18,nombre:'Dr. IQ'},
+{id:19,nombre:'Magma'},
+{id:20,nombre:'Tornado'},    
+]
